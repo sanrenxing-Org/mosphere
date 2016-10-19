1d5e360ee56d75d07fa479aae4a566762efa6429
ypf <ypf@qq.com>
2016-10-19 18:28:25 +0800

    修改关于页

--- views/site/about.php
+++ views/site/about.php
@@ -7,12 +7,24 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<style>
+.team{margin-top:15px;}
+.avatar{ margin:15px auto} 
+.avatar img{ border-radius:50%}
+</style>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        We are devoted to share everything about music,movies,games and photograph. We tend to the idea that be interested,be interesting.
     </p>
-
-    <code><?= __FILE__ ?></code>
+	<div class="team">
+		<div class="avatar">
+			<img src="/mosphere/web/statics/images/profile/avatar/default.jpg">
+			<span>mos,站长，负责网站的开发与设计</span>
+		</div>
+		<div class="avatar"><img src="/mosphere/web/statics/images/profile/avatar/default.jpg"></div>
+		<div class="avatar"><img src="/mosphere/web/statics/images/profile/avatar/default.jpg"></div>
+		<div class="avatar"><img src="/mosphere/web/statics/images/profile/avatar/default.jpg"></div>
+	</div>
 </div>
